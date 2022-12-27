76958e11e0b94bf10dc2b3036ba5cbc14aa39e6a
dhiransapkota45 <dhiransapkota55@gmail.com>
2022-12-27 16:52:49 +0545

    about page added

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -8,8 +8,10 @@ const Hero = () => {
     <>
       <div className="max-w padding h-screen border-2 ">
         <div className="w-full h-full md:flex justify-between items-center">
-          <div className=" flex flex-col  ">
-            <span className=" text-6xl">{greetings}</span><span>{name}</span><span>{work}</span>
+          <div className=" flex flex-col text-solid-heading font-bold text-6xl  ">
+            <span className=" ">{greetings}</span>
+            <span className=" max-w-lg">{name}</span>
+            <span>{work}</span>
           </div>
           <div className=" ">
             <Image src="/static/images/profile.jpg" width="300" height="300" alt="profile_pic" />
