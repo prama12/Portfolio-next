5aeee0844094e07df8d572455f69a996759bb719
dhiransapkota45 <dhiransapkota55@gmail.com>
2022-12-27 15:09:58 +0545

    image folder added

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
+import herodata from "../data/herodata";
 
 const Hero = () => {
-  return <>hi i am hero</>;
+  const { greetings, name, work } = herodata
+  return (
+    <>
+      <div className="max-w padding h-screen border-2 ">
+        <div className="w-full h-full md:flex justify-between items-center">
+          <div className=" flex flex-col  ">
+            <span className=" text-6xl">{greetings}</span><span>{name}</span><span>{work}</span>
+          </div>
+          <div className=" ">
+            <Image src="/static/images/profile.jpg" width="300" height="300" alt="profile_pic" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Hero from "../components/home/Hero";
 
 export default function Home() {
   return (
@@ -9,7 +10,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-screen bg-black text-white">prama</div>
+      {/* <div className="h-screen bg-black text-white">prama</div> */}
+      <Hero />
     </>
   );
 }
