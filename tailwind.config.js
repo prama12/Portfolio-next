991a227c943de0e18467b56f3574df254b575fa9
prama12 <panditprama@gmail.com>
2022-12-27 13:28:42 +0545

    custom color add

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,19 @@ module.exports = {
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#666666",
+        "primary-light": "#A7A7A7",
+        "solid-heading": "#42446E",
+        "gradient-heading1": "#13B0F5",
+        "gradient-heading2": "#E70FAA",
+        "primary-button": "#D7FFE0",
+        "primary-button-text": "#018C0F",
+        "primary-dark-mode": "#CCCCCC",
+        "dark-background": "#363636",
+      },
+    },
   },
   plugins: [],
 };
