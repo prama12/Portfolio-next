76c12a733fac4f25e6f4509cc1e04839e68479a1
prama12 <panditprama@gmail.com>
2022-12-27 16:47:45 +0545

    add font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,13 @@ module.exports = {
   theme: {
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"],
+      "Open Sans": ["Open Sans", "sans-serif"],
     },
     extend: {
       colors: {
         "primary-dark": "#666666",
-        "primary-light": "#A7A7A7",
-        "solid-heading": "#42446E",
-        "gradient-heading1": "#13B0F5",
-        "gradient-heading2": "#E70FAA",
-        "primary-button": "#D7FFE0",
-        "primary-button-text": "#018C0F",
-        "primary-dark-mode": "#CCCCCC",
-        "dark-background": "#363636",
+        "dark-background": "#111111",
+        "primary-yellow": "#ffb400",
       },
     },
   },
