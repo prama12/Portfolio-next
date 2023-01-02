c81ebf2e48790e6becf97a6455a7c40775bab788
dhiransapkota45 <dhiransapkota55@gmail.com>
2023-01-02 16:41:24 +0545

    commit

--- components/data/projectdata.js
+++ components/data/projectdata.js
@@ -3,8 +3,8 @@ const projectdata = [
     imagelink: "/static/images/ecommerce.png",
     title: "ecommerce",
     description: "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
-    link: "",
-    githublink: "",
+    link: "https://ecommerce-dhiran.netlify.app/",
+    githublink: "https://github.com/dhiransapkota45/ecommerece",
     category: "full stack",
   },
   {
