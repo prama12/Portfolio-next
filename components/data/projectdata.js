846cf7fbe73dc18027c631b8d8bf3b60f57718e0
prama12 <panditprama@gmail.com>
2023-01-03 16:56:43 +0545

    adding projects

--- components/data/projectdata.js
+++ components/data/projectdata.js
@@ -2,52 +2,30 @@ const projectdata = [
   {
     imagelink: "/static/images/ecommerce.png",
     title: "ecommerce",
-    description: "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
-    link: "https://ecommerce-dhiran.netlify.app/",
-    githublink: "https://github.com/dhiransapkota45/ecommerece",
+    description:
+      "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
+    link: "https://ecommerce-websitedemo.netlify.app/",
+    githublink: "https://github.com/prama12/EcommerceSite-redux-toolkit",
     category: "full stack",
   },
   {
-    imagelink: "/static/images/ecommerce.png",
-    title: "ecommerce",
-    description: "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
-    link: "",
+    imagelink: "/static/images/gym.png",
+    title: "Gym Management",
+    description:
+      "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
+    link: "https://gym-management-system.netlify.app/",
     githublink: "",
     category: "front end",
   },
   {
-    imagelink: "/static/images/ecommerce.png",
-    title: "ecommerce",
-    description: "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
-    link: "",
-    githublink: "",
-    category: "backend",
-  },
-  {
-    imagelink: "/static/images/ecommerce.png",
-    title: "ecommerce",
-    description: "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
-    link: "",
-    githublink: "",
-    category: "design",
-  },
-  {
-    imagelink: "/static/images/ecommerce.png",
-    title: "ecommerce",
-    description: "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
-    link: "",
-    githublink: "",
-    category: "full stack",
-  },
-  {
-    imagelink: "/static/images/ecommerce.png",
-    title: "ecommerce",
-    description: "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
-    link: "",
+    imagelink: "/static/images/gaming.png",
+    title: "gaming site",
+    description:
+      "The sun was setting on the horizon, casting a warm glow over the small town nestled in the valley below. The streets were bustling with people making their way home from work, chatting and laughing as they went.",
+    link: "https://armancards.com/",
     githublink: "",
     category: "full stack",
   },
 ];
 
-export default projectdata
-
+export default projectdata;
