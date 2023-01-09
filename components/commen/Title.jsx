0130469d14eafae699df8e6cf624ccce7d286481
prama12 <panditprama@gmail.com>
2023-01-09 15:32:20 +0545

    heading add

--- components/commen/Title.jsx
+++ components/commen/Title.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Title = ({ firstdata, seconddata, backgroundtext }) => {
   return (
     <div className="relative flex justify-center items-center overflow-hidden">
-      <div className=" relative text-7xl md:text-[110px] font-extrabold  tracking-wider text-primary-dark dark:text-primary-light opacity-10 uppercase">
+      <div className=" relative text-5xl sm:text-7xl mdd:text-[110px] leading-[77px] font-Poppins font-bold tracking-[10px] text-primary-dark dark:text-primary-light opacity-10 dark:opacity-20 uppercase">
         {backgroundtext}
       </div>
-      <div className="absolute text-4xl sm:text-6xl font-extrabold tracking-wide text-primary-dark dark:text-primary-light uppercase ">
+      <div className="absolute text-3xl mdd:text-[55px] font-bold tracking-wide text-primary-dark font-Poppins dark:text-primary-light uppercase ">
         <span>{firstdata}</span>
         <span className=" text-primary-yellow uppercase">{seconddata}</span>
       </div>
