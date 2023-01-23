412c5511593d44d2e68a3303bdd76791f4a3140b
prama12 <panditprama@gmail.com>
2023-01-23 23:00:31 +0545

    email set

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -1,34 +1,39 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import Button from "../commen/Button";
 import Telegram from "../../public/static/icons/telegram-plane.svg";
 import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-  const form = useRef();
+  const [formState, setFormState] = useState({});
 
   const sendEmail = (e) => {
     e.preventDefault();
+    const config = {
+      SecureToken: "3ae5ade4-73b6-4c9a-b3fe-9942e2169fda",
+      To: "panditprama@gmail.com",
+      From: formState.email,
+      Subject: formState.sub,
+      Body: formState.message,
+    };
 
-    emailjs
-      .sendForm(
-        "service_ybk3ctk",
-        "template_t52cv3n",
-        form.current,
-        "_oOCAs9jSLHEZ-4TW"
-      )
-      .then(
+    if (window.Email) {
+      window.Email.send(config).then(
         (result) => {
-          console.log("message sent");
+          alert("message sent successfully");
         },
         (error) => {
-          console.log("message can't sent");
+          alert("message can't sent");
         }
       );
+    }
+  };
+
+  const changeHandler = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
   };
 
   return (
     <form
-      ref={form}
       onSubmit={sendEmail}
       className=" py-10 grid gap-5 p-6 text-primary-dark dark:text-primary-light animation"
     >
@@ -37,7 +42,9 @@ const ContactForm = () => {
           className="input rounded-full "
           placeholder="YOUR NAME"
           type="text"
-          name="user_name"
+          name="name"
+          value={formState.name || ""}
+          onChange={changeHandler}
         />
       </div>
       <div className="z-[999] col-span-2 sm:col-span-1 ">
@@ -45,7 +52,9 @@ const ContactForm = () => {
           className=" input rounded-full"
           placeholder="YOUR EMAIL"
           type="email"
-          name="user_email"
+          name="email"
+          value={formState.email || ""}
+          onChange={changeHandler}
         />
       </div>
       <div className="z-[999] col-span-2 ">
@@ -53,6 +62,8 @@ const ContactForm = () => {
           className="input rounded-full"
           placeholder="YOUR SUBJECT"
           type="text"
+          value={formState.sub || ""}
+          onChange={changeHandler}
           name="sub"
         />
       </div>
@@ -61,6 +72,8 @@ const ContactForm = () => {
           className=" input rounded-3xl h-32"
           placeholder="YOUR MESSAGE"
           name="message"
+          value={formState.message || ""}
+          onChange={changeHandler}
         />
       </div>
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import Layout from "../components/layout/Layout";
 import Theme from "../context/ThemeContext";
 import "../styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
         <Layout>
+          <Script src="https://smtpjs.com/v3/smtp.js" />
           <Component {...pageProps} />
         </Layout>
       </Theme>
