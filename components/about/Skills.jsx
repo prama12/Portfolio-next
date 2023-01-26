4de21324e27af88a71505dd5125c9dc10c4e58ad
prama12 <panditprama@gmail.com>
2023-01-26 23:04:29 +0545

    changes

--- components/about/Skills.jsx
+++ components/about/Skills.jsx
@@ -14,6 +14,10 @@ const Skills = () => {
           {skills.map((data, index) => {
             console.log("skill", data);
             return <ProgressBar key={index} {...data} />;
+
+
+
+            
           })}
         </div>
       </div>
