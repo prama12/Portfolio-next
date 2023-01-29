f56dd064a34176103454642379ef2d86b386a2e3
prama12 <panditprama@gmail.com>
2023-01-29 15:17:11 +0545

    adding space

--- components/about/Skills.jsx
+++ components/about/Skills.jsx
@@ -12,9 +12,6 @@ const Skills = () => {
 
         <div className=" grid grid-cols-2 px-5 xsm:px-10 xxsm:grid-cols-3 md:grid-cols-4 gap-10 xsm:gap-x-24 md:gap-20 lg:gap-x-32 w-full ">
           {skills.map((data, index) => {
-            {
-              /* console.log("skill", data); */
-            }
             return <ProgressBar key={index} {...data} />;
           })}
         </div>
