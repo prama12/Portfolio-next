8e893c38551445ae2a446823e4f874773d24c6d8
prama12 <panditprama@gmail.com>
2023-01-29 11:44:30 +0545

    arrange progressbar

--- components/about/Skills.jsx
+++ components/about/Skills.jsx
@@ -5,14 +5,16 @@ import skills from "../data/skills";
 const Skills = () => {
   return (
     <>
-      <div className="max-w-[1000px] mx-auto flex flex-col gap-5 sm:gap-10">
+      <div className=" mx-auto w-full lg:w-[990px] xl:w-[1100px] flex flex-col gap-5 sm:gap-10">
         <div className=" flex justify-center">
           <Header title="MY SKILLS" />
         </div>
 
-        <div className=" grid grid-cols-2 px-5 xsm:px-10 md:grid-cols-4 gap-20 md:gap-10 lg:gap-20 w-full ">
+        <div className=" grid grid-cols-2 px-5 xsm:px-10 xxsm:grid-cols-3 md:grid-cols-4 gap-10 xsm:gap-x-24 md:gap-20 lg:gap-x-32 w-full ">
           {skills.map((data, index) => {
-            console.log("skill", data);
+            {
+              /* console.log("skill", data); */
+            }
             return <ProgressBar key={index} {...data} />;
           })}
         </div>
--- components/commen/ProgressBar.jsx
+++ components/commen/ProgressBar.jsx
@@ -3,32 +3,36 @@ import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 const ProgressBar = ({ name, percent }) => {
   return (
-    <VisibilitySensor>
-      {({ isVisible }) => {
-        const percentage = isVisible ? percent : 0;
-        return (
-          <div className="w-fit flex gap-3 flex-col items-center justify-center">
-            <CircularProgressbar
-              styles={buildStyles({
-                rotation: 0.25,
-                strokeLinecap: "butt",
-                textSize: "12px",
-                pathTransitionDuration: 1,
-                pathColor: `#ffb400`,
-                textColor: "#666666",
-                trailColor: "#d6d6d6",
-                backgroundColor: "#3e98c7",
-              })}
-              value={percentage}
-              text={`${percentage}%`}
-            />
-            <div className="font-medium uppercase text-xs sm:text-sm md:text-base text-primary-dark dark:text-primary-light">
-              {name}
+    <>
+    
+
+      <VisibilitySensor>
+        {({ isVisible }) => {
+          const percentage = isVisible ? percent : 0;
+          return (
+            <div className="w-full flex gap-3  flex-col items-center justify-center">
+              <CircularProgressbar
+                styles={buildStyles({
+                  rotation: 0.25,
+                  strokeLinecap: "butt",
+                  textSize: "12px",
+                  pathTransitionDuration: 1,
+                  pathColor: `#ffb400`,
+                  textColor: "#666666",
+                  trailColor: "#d6d6d6",
+                  backgroundColor: "#3e98c7",
+                })}
+                value={percentage}
+                text={`${percentage}%`}
+              />
+              <div className="font-medium uppercase text-xs sm:text-sm md:text-base text-primary-dark dark:text-primary-light">
+                {name}
+              </div>
             </div>
-          </div>
-        );
-      }}
-    </VisibilitySensor>
+          );
+        }}
+      </VisibilitySensor>
+    </>
   );
 };
 
