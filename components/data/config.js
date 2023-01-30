a993baca6e3f9a874e0c1348f4f4b79fa8ed8159
prama12 <panditprama@gmail.com>
2023-01-30 11:04:17 +0545

    delete config file

--- components/data/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  SecureToken: "3ae5ade4-73b6-4c9a-b3fe-9942e2169fda",
-  To: "them@website.com",
-  From: "you@isp.com",
-  Subject: "This is the subject",
-  Body: "And this is the body",
-};
