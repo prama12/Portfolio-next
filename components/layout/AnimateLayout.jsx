400b581b45f30d1b8662d0c1b265f0619dcdd34f
prama12 <panditprama@gmail.com>
2023-01-30 14:08:51 +0545

    delete code from animateLayout

--- components/layout/AnimateLayout.jsx
+++ components/layout/AnimateLayout.jsx
@@ -2,22 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const AnimateLayout = ({ children }) => {
   const [animator, setAnimator] = useState(false);
-  // const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  // useEffect(() => {
-  //   function handleMouseMove(event) {
-  //     setMousePosition({
-  //       x: event.clientX,
-  //       y: event.clientY,
-  //     });
-  //   }
-
-  //   window.addEventListener("mousemove", handleMouseMove);
-
-  //   return () => {
-  //     window.removeEventListener("mousemove", handleMouseMove);
-  //   };
-  // }, []);
   useEffect(() => {
     setAnimator(true);
   }, []);
@@ -33,15 +18,6 @@ const AnimateLayout = ({ children }) => {
             animator ? "opacity-100 pt-0" : "opacity-0 pt-20"
           } delay-700 duration-1000`}
         >
-          {/* <div
-            style={{
-              position: "fixed",
-              top: mousePosition.y - 16,
-              left: mousePosition.x - 16,
-            }}
-            className="w-8 h-8 rounded-full bg-primary-yellow hidden xl:block opacity-70 duration-300 ease-out"
-          ></div> */}
-
           {children}
         </div>
       </div>
