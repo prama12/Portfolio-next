7252f81303d7f66a6f10814c6878a9199860c970
prama12 <panditprama@gmail.com>
2023-01-31 12:32:24 +0545

    toast add

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -1,7 +1,9 @@
-import Button from "../commen/Button";
-import Telegram from "../../public/static/icons/telegram-plane.svg";
 import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import Telegram from "../../public/static/icons/telegram-plane.svg";
+import toast, { Toaster } from "react-hot-toast";
+import Button from "../commen/Button";
+import Tick from "../../public/static/icons/tick.svg";
 
 const ContactForm = () => {
   const [formState, setFormState] = useState({});
@@ -19,11 +21,22 @@ const ContactForm = () => {
       )
       .then(
         (result) => {
-          alert("Message sent successfully");
+          toast.custom((t) => (
+            <div
+              className={`${
+                t.visible ? "animate-enter" : "animate-leave"
+              } bg-primary-light border flex items-center gap-3 dark:bg-dark-background rounded-lg shadow-sm text-primary-dark font-Poppins font-semibold text-sm dark:text-primary-light border-primary-yellow px-4 py-3`}
+            >
+              <div className="w-5 h-5 rounded-full flex items-center justify-center text-sm text-primary-light font-Poppins font-semibold bg-primary-yellow">
+                <Tick />
+              </div>
+              <div>Message sent successfully</div>
+            </div>
+          ));
           setFormState("");
         },
         (error) => {
-          console.log("message does't sent");
+          toast.error("message does't sent");
           setFormState("");
         }
       );
@@ -33,60 +46,62 @@ const ContactForm = () => {
     setFormState({ ...formState, [e.target.name]: e.target.value });
   };
   return (
-    <form
-      ref={form}
-      onSubmit={sendEmail}
-      className="  w-full grid gap-5 text-primary-dark dark:text-primary-light animation"
-      autocomplete="off"
-    >
-      <div className=" z-[999]  col-span-2 sm:col-span-1 ">
-        <input
-          required
-          className="input capitalize rounded-full "
-          placeholder="YOUR NAME"
-          type="text"
-          name="name"
-          value={formState.name || ""}
-          onChange={changeHandler}
-        />
-      </div>
-      <div className="z-[999] col-span-2 sm:col-span-1">
-        <input
-          required
-          className=" input rounded-full"
-          placeholder="YOUR EMAIL"
-          type="email"
-          name="email"
-          value={formState.email || ""}
-          onChange={changeHandler}
-        />
-      </div>
-      <div className="z-[999] col-span-2 ">
-        <input
-          required
-          className="input rounded-full"
-          placeholder="YOUR SUBJECT"
-          type="text"
-          value={formState.subject || ""}
-          onChange={changeHandler}
-          name="subject"
-        />
-      </div>
-      <div className="z-[999] col-span-2 ">
-        <textarea
-          required
-          className=" input rounded-3xl h-32"
-          placeholder="YOUR MESSAGE"
-          name="message"
-          value={formState.message || ""}
-          onChange={changeHandler}
-        />
-      </div>
+    <>
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="  w-full grid gap-5 text-primary-dark dark:text-primary-light animation"
+      >
+        <div className=" z-[999]  col-span-2 sm:col-span-1 ">
+          <input
+            required
+            className="input capitalize rounded-full "
+            placeholder="YOUR NAME"
+            type="text"
+            name="name"
+            value={formState.name || ""}
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="z-[999] col-span-2 sm:col-span-1">
+          <input
+            required
+            className=" input rounded-full"
+            placeholder="YOUR EMAIL"
+            type="email"
+            name="email"
+            value={formState.email || ""}
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="z-[999] col-span-2 ">
+          <input
+            required
+            className="input rounded-full"
+            placeholder="YOUR SUBJECT"
+            type="text"
+            value={formState.subject || ""}
+            onChange={changeHandler}
+            name="subject"
+          />
+        </div>
+        <div className="z-[999] col-span-2 ">
+          <textarea
+            required
+            className=" input rounded-3xl h-32"
+            placeholder="YOUR MESSAGE"
+            name="message"
+            value={formState.message || ""}
+            onChange={changeHandler}
+          />
+        </div>
 
-      <div className=" w-fit">
-        <Button text="send message" icon={<Telegram />} />
-      </div>
-    </form>
+        <div className=" w-fit">
+          <Button text="send message" icon={<Telegram />} />
+          <Toaster position="bottom-right" reverseOrder={false} />
+        </div>
+      </form>
+    </>
   );
 };
 
